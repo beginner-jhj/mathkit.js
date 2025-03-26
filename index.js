cadc89f3987cc34145c4f1dafb62388aaf17145f
jhj <hj5014221@gmail.com>
2025-03-26 18:30:42 +0900

    fix buges

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import * as numberOperation from "numberOperation.js";
-import * as trigonometry from "trigonometric.js";
-import * as combinatorics from "combinatorics.js";
+import * as numberOperation from "./numberOperation.js";
+import * as trigonometry from "./trigonometric.js";
+import * as combinatorics from "./combinatorics.js";
 
 export default {
   ...numberOperation,
